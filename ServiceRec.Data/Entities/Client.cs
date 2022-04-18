780dd7ff15776b377ade54f7842abdecc2f9b7ff
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2022-04-18 16:26:18 -0400

    feat: Implemented DataAnnotations

--- ServiceRec.Data/Entities/Client.cs
+++ ServiceRec.Data/Entities/Client.cs
@@ -1,6 +1,8 @@
 ﻿using ServiceRec.Data.Entities.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +11,23 @@ namespace ServiceRec.Data.Entities
 {
     public class Client
     {
+        [Key]
         public int Id { get; set; }
+        [DisplayName("Client Name")]
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [StringLength(50, ErrorMessage = "Field {0} must be between {2} and {1} characters")]
         public string ClientName { get; set; }
+        [DisplayName("Email")]
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [StringLength(50, ErrorMessage = "Field {0} must be between {2} and {1} characters")]
         public string Email { get; set; }
+        [DisplayName("Created At")]
+        [DataType(DataType.DateTime)]
         public DateTime CreatedAt { get; set; }
+        [DisplayName("Status")]
         public Status Status { get; set; }
         public int ServiceId { get; set; }
+        [DisplayName("Service")]
         public Service Service { get; set; }
     }
 }
--- ServiceRec.Data/Entities/Service.cs
+++ ServiceRec.Data/Entities/Service.cs
@@ -1,6 +1,8 @@
 ﻿using ServiceRec.Data.Entities.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,18 @@ namespace ServiceRec.Data.Entities
 {
     public class Service
     {
+        [Key]
         public int Id { get; set; }
+        [DisplayName("Service Name")]
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [StringLength(50, ErrorMessage = "Field {0} must be between {2} and {1} characters")]
         public string ServiceName { get; set; }
+        [DisplayName("Price")]
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [StringLength(50, ErrorMessage = "Field {0} must be between {2} and {1} characters")]
         public decimal Price { get; set; }
+        [DisplayName("Type")]
+        [StringLength(30, ErrorMessage = "Field {0} must be between {2} and {1} characters")]
         public ServiceType Type { get; set; }
     }
 }
